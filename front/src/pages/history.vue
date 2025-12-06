f0c8f9a37a8dcca840dc7e2e637b655d85b71420
Artur Mędrygał <medrygal.artur@gmail.com>
2025-12-06 18:22:09 +0100

    front: history

--- front/src/pages/history.vue
+++ front/src/pages/history.vue
@@ -1,9 +1,21 @@
 <template>
-  <Layout> <div class="history">History</div> </Layout>
+  <Layout>
+    <div class="history">{{ data }}, {{ isLoading }}</div>
+  </Layout>
 </template>
 
 <script setup lang="ts">
+import { httpService } from '@/api'
 import Layout from '@/components/Layout.vue'
+import { useQuery } from '@tanstack/vue-query'
+
+const { data, isLoading } = useQuery({
+  queryKey: ['get-visits'],
+  queryFn: async () => {
+    const { data } = await httpService.GET('/visits')
+    return data
+  },
+})
 </script>
 
 <style lang="scss" scoped>
