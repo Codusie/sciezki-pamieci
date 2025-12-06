490163810b5eadfa4b4dbe61fee6f494de566f95
Artur Mędrygał <medrygal.artur@gmail.com>
2025-12-06 14:47:42 +0100

    feat: Frontend setup

--- front/src/App.vue
+++ front/src/App.vue
@@ -6,6 +6,7 @@
     Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
     documentation
   </p>
+  <Button>Button</Button>
 </template>
 
-<style scoped></style>
+<style scoped lang="scss"></style>
--- front/src/main.ts
+++ front/src/main.ts
@@ -1,14 +1,24 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import { VueQueryPlugin } from '@tanstack/vue-query'
+import PrimeVue from 'primevue/config'
 
 import App from './App.vue'
 import router from './router'
+
+import '@/assets/styles/index.scss'
+import { Theme } from './theme'
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
 app.use(VueQueryPlugin)
+app.use(PrimeVue, {
+  theme: {
+    preset: Theme,
+    darkModeSelector: false,
+  },
+})
 
 app.mount('#app')
